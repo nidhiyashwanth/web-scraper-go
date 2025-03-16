1b30b84781f3584af6ce78ca8773ec0587b8609a
nidhiyashwanth <nidhiyashwanth007@gmail.com>
2025-03-17 01:16:09 +0530

    write a simple scraper to fetch product url, image, name and price

--- scraper.go
+++ scraper.go
@@ -1,8 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"log"
+	"os"
+
+	"github.com/gocolly/colly"
 )
+
+type Product struct {
+	Url, Image, Name, Price string
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
+	var products []Product
+
+	c.OnHTML("li.product", func(h *colly.HTMLElement) {
+		// Scraping logic
+		product := Product{}
+		product.Url = h.ChildAttr("a", "href")
+		product.Image = h.ChildAttr("img", "src")
+		product.Name = h.ChildText(".product-name")
+		product.Price = h.ChildText(".price")
+
+		products = append(products, product)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		file, err := os.Create("products.csv")
+		if err != nil {
+			log.Fatal("Failed to create file", err)
+		}
+		defer file.Close()
+		writer := csv.NewWriter(file)
+
+		// Write the csv headers
+		headers := []string{"Url", "Image", "Name", "Price"}
+
+		writer.Write(headers)
+
+		for _, product := range products {
+			record := []string{
+				product.Url,
+				product.Image,
+				product.Name,
+				product.Price,
+			}
+			writer.Write(record)
+		}
+		defer writer.Flush()
+
+	})
+
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
+
 }
